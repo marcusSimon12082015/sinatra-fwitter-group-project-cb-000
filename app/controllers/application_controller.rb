678031364199cb45dbd02daa80c470b18759d950
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-01-27 14:44:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < Sinatra::Base
 
   get '/tweets' do
     binding.pry
-    if session.key?(:id)
+    if session.key?(:id) && !session[:id].empty?
       #we look for a user with and id only if the key exists in the session hash
       @user = User.find(session[:id])
       if @user.nil?
